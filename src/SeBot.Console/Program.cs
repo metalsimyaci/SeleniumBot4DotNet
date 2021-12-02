708be030a724c3b11e7a3e60c1a3af0e7145bcd9
Hasan URAL <metalsimyaci@outlook.com>
2021-12-02 14:15:51 +0300

    Temel bot çalışması

---
+++ src/SeBot.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeBot.Console
+{
+    public class Program
+    {
+        public const string DRIVER_PATH = @"D:\Tools\\ChromeDriver\";
+        public const string DRIVER_VERSION = @"96.0.4664.45";
+
+        public static void Main(string[] args)
+        {
+            var driver = new ChromeDriver($@"{DRIVER_PATH}{DRIVER_VERSION}\");
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://google.com");
+                driver.Manage().Window.Maximize();
+                var textBox = driver.FindElement(By.CssSelector(@"input[type=Text]"));
+                if(textBox != null)
+                {
+                    textBox.SendKeys("Hasan URAL");
+                    textBox.Submit();
+                }
+
+                System.Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                driver?.Close();
+            }
+        }
+
+
+    }
+
+}
---
+++ src/SeBot.Core/SeBot.Core.proj/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SeBot.Core
+{
+    public class Class1
+    {
+    }
+}
